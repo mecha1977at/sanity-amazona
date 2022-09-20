97651dfd01c1e5051fddd43397a0e8acacc582fb
mecha1977at <mecha1977at@gmail.com>
2022-09-20 13:14:24 +0200

    Video-12-Display-Cart-Badge-In-Header-Menu

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import { Alert, CircularProgress, Grid, Typography } from '@mui/material';
+import axios from 'axios';
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useSnackbar } from 'notistack';
+import { useEffect, useState, useContext } from 'react';
 import ProductItem from '../components/ProductItem';
 import client from '../utils/client';
+import { urlForThumbnail } from '../utils/image';
+import { Store } from '../utils/Store';
 import Layout from './../components/Layout';
-// import ProductItem from './../components/ProductItem';
 
 export default function Home() {
+  const {
+    state: { cart },
+    dispatch,
+  } = useContext(Store);
+  const router = useRouter();
+  const { enqueueSnackbar } = useSnackbar();
   const [state, setState] = useState({
     products: [],
     error: '',
@@ -25,6 +35,31 @@ export default function Home() {
     };
     fetchData();
   }, []);
+  const addToCartHandler = async (product) => {
+    const existItem = cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      enqueueSnackbar('Sorry. Product is out of stock', { variant: 'error' });
+      return;
+    }
+    dispatch({
+      type: 'CART_ADD_ITEM',
+      payload: {
+        _key: product._id,
+        name: product.name,
+        countInStock: product.countInStock,
+        slug: product.slug.current,
+        price: product.price,
+        image: urlForThumbnail(product.image),
+        quantity,
+      },
+    });
+    enqueueSnackbar(`${product.name} added to the cart`, {
+      variant: 'success',
+    });
+    router.push('/cart');
+  };
   return (
     <Layout>
       {loading ? (
@@ -35,7 +70,10 @@ export default function Home() {
         <Grid container spacing={3}>
           {products.map((product) => (
             <Grid item md={4} key={product.slug}>
-              <ProductItem product={product}></ProductItem>
+              <ProductItem
+                product={product}
+                addToCartHandler={addToCartHandler}
+              ></ProductItem>
             </Grid>
           ))}
         </Grid>
